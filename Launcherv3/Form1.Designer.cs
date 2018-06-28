c2322e5830123db7544eea5bb0b81b99cf99f3b2
asandru <artister.hd@gmail.com>
2018-06-28 13:42:43 +0100

    add application icon cleaned up build
    Signed-off-by: asandru <artister.hd@gmail.com>

--- Launcherv3/Form1.Designer.cs
+++ Launcherv3/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
@@ -357,6 +358,7 @@
             this.ClientSize = new System.Drawing.Size(847, 428);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
